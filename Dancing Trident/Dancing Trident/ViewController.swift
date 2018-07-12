e2d2afea8a55e8912bb73487aef9f1c73a691f61
Hal Mueller <hal@mobilegeographics.com>
2018-07-11 19:05:21 -0700

    Launch forestdb_dump as subprocess! Yay!

--- Dancing Trident/Dancing Trident/ViewController.swift
+++ Dancing Trident/Dancing Trident/ViewController.swift
@@ -16,6 +16,22 @@ class ViewController: NSViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tryIt(_ sender: Any) {
+        if let path = Bundle.main.path(forAuxiliaryExecutable: "bin/forestdb_dump") {
+            print(path)
+            let executableURL = URL(fileURLWithPath: path)
+            let option = "--hex-body"
+            let sampleDBFile = "/Users/hal/DevelopmentSandbox/TridentTools/files/data/s_da7b5973-9f0e-4ced-a138-b024eee51be7.cblite2/db.forest.0"
+            do { try
+                Process.run(executableURL, arguments: [option, sampleDBFile]) { (proc) in
+                    print(proc)
+                }
+            } catch {
+                print("oops")
+            }
+        }
+    }
+
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
