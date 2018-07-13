3ead80516821b9a9b4fce8a445243004ef17470a
Hal Mueller <hal@mobilegeographics.com>
2018-07-13 16:43:44 -0700

    new Dancing Trident project

---
+++ Dancing Trident/Dancing Trident/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dancing Trident
+//
+//  Created by Hal Mueller on 7/13/18.
+//  Copyright © 2018 Hal Mueller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
